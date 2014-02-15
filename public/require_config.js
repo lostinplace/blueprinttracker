0f75b5dffe7d8be9b4aff01e5b5da461c4fa0b56
Dylan Spencer <dspencer@zipcar.com>
2014-02-15 11:01:43 -0500

    Fixed node install and require config

--- public/require_config.js
+++ public/require_config.js
@@ -1,10 +1,11 @@
 requirejs.config({
   paths:{
     'handlebars': 'handlebars/handlebars',
-    'jquery':'jquery/jquery',
+    'jquery':'jquery/dist/jquery',
     'lodash' : 'lodash/dist/lodash',
     'backbone' : 'backbone/backbone',
-    'text' : 'text/text'
+    'text' : 'text/text',
+    'underscore' : 'underscore/underscore'
   },
   shim: {
     'handlebars': {
