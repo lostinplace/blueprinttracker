a498caaf15480c8447ed19130f06c4532e3cce0f
Dylan Spencer <dspencer@zipcar.com>
2014-02-15 18:06:27 -0500

    Added parser to get character info from keys. Needs cleaned up.

--- app.js
+++ app.js
@@ -40,6 +40,7 @@ app.get('tests', function(req, res){
 
 app.get('/users', user.list);
 app.get('/data/blueprints', data.blueprints);
+app.get('/data/characters', data.characters);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/data.js
+++ routes/data.js
@@ -1,5 +1,41 @@
 var csv = require('csv'),
-  _ = require('lodash');
+    _ = require('lodash'),
+    neow = require('neow'),
+    fs = require('fs'),
+    Q = require('q');
+
+
+exports.characters = function(req,res){
+  'use strict';
+
+  var output = [],
+      promises = [];
+ 
+  //Replace these lines when we get proper key storage
+  //api_keys is an array of objects with keyId and vCode attributes
+  var file = fs.readFileSync('secure_data/api_keys.json',{'encoding':'utf8'}),
+      apiKeys = JSON.parse(file).accounts;
+
+  for(var index in apiKeys){
+    var apiKey = apiKeys[index],
+        client = new neow.EveClient({
+          keyID: apiKey.keyId,
+          vCode: apiKey.vCode
+        });
+
+    promises.push(client.fetch('account:Characters')
+      .then(function(result){
+        for(var characterID in result.characters){
+          output.push(result.characters[characterID]);
+        }
+      }));
+  }
+
+  Q.all(promises).then(function(){
+      res.send(output);
+  });
+};
+
 
 exports.blueprints = function(req,res){
   'use strict';
